573832cd42557cdd5e31d2ef2e2bdfdb14fc6b31
Jose Aparecido <j.ferreira@sodexo.com>
2025-02-25 20:01:04 -0300

    feat: Add gitlab version

---
+++ src/main/java/com/zelabs/java_ecs/JavaEcsApplication.java
@@ -0,0 +1,13 @@
+package com.zelabs.java_ecs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEcsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zelabs/java_ecs/JavaEcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zelabs.java_ecs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
